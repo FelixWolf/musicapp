a3e32656c0e2af0fb652bb63de651d5f41e4629b
Kyler Eastridge <felix.wolfz@gmail.com>
2023-09-23 17:33:47 -0400

    Add static serving for when running in debug mode

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -31,4 +33,4 @@ urlpatterns = [
     path('get/', include('get.urls')),
     path('search/', include('search.urls')),
     path('api/', include('api.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
